185c3e800970c660ea91d99f09ffd9f4813377a9
Queniex <queniesalbiyah@gmail.com>
2022-11-15 19:20:19 +0700

    rename file

---
+++ autoloading/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// require_once 'APP/Produk/InfoProduk.php';
+// require_once 'APP/Produk/Produk.php';
+// require_once 'APP/Produk/Komik.php';
+// require_once 'APP/Produk/Game.php';
+// require_once 'APP/Produk/CetakInfoProduk.php'; // Atau bisa dipindahkan ke file lain, jd tinggal manggil 1 file
+
+require_once 'APP/Produk/init.php';
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk1);
+echo $cetakProduk->cetak();
+
+?>
